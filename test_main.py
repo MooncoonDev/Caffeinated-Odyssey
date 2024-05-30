7f44388cb52f2e231823f22526c135802ad9b282
Marcel Coetzee <marcel@mooncoon.com>
2024-05-30 18:15:11 +0200

    Refactor code for stylistic improvements

--- test_main.py
+++ test_main.py
@@ -6,7 +6,10 @@ from main import get_client_application, get_worker_application
 def test_create_order():
     client_app = get_client_application()
     client = TestClient(client_app)
-    response = client.post("/order", json={"name": "americano", "description": "yum", "price": 4.00})
+    response = client.post(
+        "/order",
+        json={"name": "americano", "description": "yum", "price": 4.00},
+    )
     assert response.status_code == 200
     assert response.json() == {"message": "Order received"}
 
@@ -14,16 +17,24 @@ def test_create_order():
 def test_rate_limiting():
     client_app = get_client_application()
     normal_client = TestClient(client_app)
-    ddoser_client = TestClient(client_app, base_url="http://DELUSIONAL_DDOSER_IP")
+    ddoser_client = TestClient(
+        client_app, base_url="http://DELUSIONAL_DDOSER_IP"
+    )
 
-    for i in range(10):
-        response = normal_client.post("/order", json={"name": "americano", "description": "yum", "price": 4.00})
+    for _ in range(10):
+        response = normal_client.post(
+            "/order",
+            json={"name": "americano", "description": "yum", "price": 4.00},
+        )
         assert response.status_code == 200
 
-    for i in range(11):
-        response = ddoser_client.post("/order", json={"name": "americano", "description": "yum", "price": 4.00})
+    for _ in range(11):
+        response = ddoser_client.post(
+            "/order",
+            json={"name": "americano", "description": "yum", "price": 4.00},
+        )
     assert response.status_code == 429
-    assert 'Rate limit exceeded' in response.json()['error']
+    assert "Rate limit exceeded" in response.json()["error"]
 
 
 def test_worker_flow():
@@ -32,7 +43,10 @@ def test_worker_flow():
     client = TestClient(client_app)
     worker = TestClient(worker_app)
 
-    response = client.post("/order", json={"name": "americano", "description": "yum", "price": 4.00})
+    response = client.post(
+        "/order",
+        json={"name": "americano", "description": "yum", "price": 4.00},
+    )
     assert response.status_code == 200
 
     response = worker.get("/start")
